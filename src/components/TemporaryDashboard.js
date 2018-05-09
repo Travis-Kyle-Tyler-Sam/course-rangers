5953cfc029ecee8ebcc9330c2225031c057503b2
Tyler <tylerlewisjcg@gmail.com>
2018-05-09 15:53:43 -0600

    update

--- src/components/TemporaryDashboard.js
+++ src/components/TemporaryDashboard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { getUserInfo } from './../dux/userReducer';
 import { Link } from "react-router-dom";
-
+///// this dashboard doesn't actually link anywhere yet
 
 class TemporaryDashboard extends Component {
     constructor(props) {
