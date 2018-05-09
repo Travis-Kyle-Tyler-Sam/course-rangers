c5dde13e1f526bcbd1e7227c75b77f9260546891
Tyler <tylerlewisjcg@gmail.com>
2018-05-09 15:52:30 -0600

    auth0 routing to a temporary dashboard

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from './ducks/store';
+import store from './dux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter>
       <App />
-    </HashRouter>
   </Provider>,
   document.getElementById("root")
 );
