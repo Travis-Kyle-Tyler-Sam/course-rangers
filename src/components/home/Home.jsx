ed4c9d7fb3b1e205489d5d5a365491aab6adddad
sam <sammybonfanti@gmail.com>
2018-05-10 14:32:39 -0600

    added a splash image in home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import HomeNavbar from './homeNavbar/HomeNavbar';
+import './Home.css';
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        <HomeNavbar />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <HomeNavbar/>
+                <div className="landing">
+                    <div className="dark-overlay landing-inner">
+                        <div className="container"></div>
+                    </div>
+                </div>
+
+                <div className='middle-container'>
+                    <div className='home-main-content'></div>
+                </div>
+            </div>
+        )
+    }
 }
