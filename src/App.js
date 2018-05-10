8e373dd53202f30464006ae0dca153db35ba87c6
sam <sammybonfanti@gmail.com>
2018-05-10 13:26:45 -0600

    added homepage and home top navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import StudentDashboard from './components/studentDashboard/StudentDashboard';
 import TeacherDashboard from './components/TeacherDashboard/TeacherDashboard';
 import CurriculumBuilder from './components/CurriculumBuilder/CurriculumBuilder';
 import AdminDash from './components/AdminDash/AdminDash';
+import Home from './components/home/Home';
 
 class App extends Component {
   render() {
@@ -17,10 +18,9 @@ class App extends Component {
 
        <HashRouter>
           <Switch>
-            <Route exact path='/' component={Login} />
+            <Route exact path='/' component={Home} />
             <Route path='/temporarydashboard' component={TemporaryDashboard} />
             <Route path='/studentdashboard' component={StudentDashboard} />
-            {/* <Route path='/studentdashboard' component={StudentDashboard} /> */}
             <Route path='/teacherdashboard' component={TeacherDashboard} />
             <Route path='/admindashboard' component={AdminDash} />
             <Route path='/curriculumbuilder' component={CurriculumBuilder} />
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HomeNavbar from './homeNavbar/HomeNavbar';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <HomeNavbar />
+      </div>
+    )
+  }
+}
