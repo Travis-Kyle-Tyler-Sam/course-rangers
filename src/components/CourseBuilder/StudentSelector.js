318be4734c0f78dc9d9740e404e30f711de256fd
Tyler <tylerlewisjcg@gmail.com>
2018-05-15 15:20:08 -0600

    i don't even know what the changes here are

--- src/components/CourseBuilder/StudentSelector.js
+++ src/components/CourseBuilder/StudentSelector.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import _ from 'lodash';
+import { Modal, Button, Header, Table, Pagination, Input, Form } from 'semantic-ui-react';
+
+
+
+
+
 class StudentSelector extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  
+            students: [],
+            adminID:1
+        }
     }
 
-
-    /// need a get endpoint that gets all users where access level = student
-    //// pagination 
-    //// auto complete on input
+componentDidMount(){
+}
     
     render() { 
         return ( <div>
              <h3>Students</h3>
              <input placeholder='Auto-Complete list of all students'/>
-
+             <Pagination
+                defaultActivePage={1}
+                totalPages={this.state.totalPages}
+                onPageChange={(event, data) => this.handlePage(data.activePage)}
+                />
         </div> )
     }
 }
