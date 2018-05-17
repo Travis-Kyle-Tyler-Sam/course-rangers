927f6e4087c7e8a2fff6090fa26d0032533f95e6
Kyle <kylesclawson@gmail.com>
2018-05-17 12:13:26 -0600

    latency issues

---
+++ src/components/StudentLecture/StudentLecture.js
@@ -0,0 +1,146 @@
+import React, {Component} from 'react';
+import openSocket from 'socket.io-client';
+import { Button, Input, Form, Icon, Label, List, Loader, Segment, Breadcrumb, Grid, Transition, TextArea} from 'semantic-ui-react';
+import './StudentLecture.css'
+const socket = openSocket('/');
+
+class StudentLecture extends Component {
+    constructor(){
+        super()
+        this.state = {
+            userType:'Student',
+            thumbsDisable:false,
+            studentThumbText:'',
+            studentSurveyText:'',
+            studentUnderstands:true,
+            studentsurveyinput:'',
+            userid:3,
+            classid:1,
+            teacherThumbText:'',
+            teacherSurveyText:'',
+            freeresponseinput:''
+            
+        }
+        socket.on('open thumbs', teacherinput => {
+            this.setState({
+                teacherThumbText:teacherinput,
+                thumbsDisable:false,
+                studentThumbText:'',
+                studentUnderstands:true
+            })
+        })
+        socket.on('student free response', teacherinput => {
+            this.setState({
+                teacherSurveyText:teacherinput,
+                studentSurveyText:'',
+
+            })
+        })
+    }
+    componentDidMount(){
+        const { userType, classid } = this.state;
+        socket.emit('join',`${userType}${classid}`, room => {
+            this.setState({
+                room
+            })
+        }) 
+    }
+    buttonPress = (e) => {
+        e.preventDefault()
+        const { count, userType, classid, userid} = this.state;
+        socket.emit('students send thumbs', ['thumbsup', classid, userid], () => {
+            this.setState({
+                thumbsDisable:true
+            })
+        })
+    }
+    buttonPress2 = (e) => {
+        e.preventDefault()
+        const { count2, classid, userid } = this.state;
+        socket.emit('students send thumbs',['thumbsdown', classid, userid], () => {
+            this.setState({
+                thumbsDisable:true,
+                studentUnderstands:false
+            })
+        })
+    }
+    componentWillUnmount(){
+        socket.close()
+    }
+    sendStudentResponse = (e) =>{
+        e.preventDefault()
+        const {classid, studentsurveyinput, userid} = this.state;
+        this.setState({
+            studentThumbText:studentsurveyinput
+        })
+        socket.emit('student thumb response', [studentsurveyinput, classid, userid], () => {
+            this.setState({
+                studentsurveyinput:''
+            })
+        })
+    }
+    sendFreeResponse = (e) => {
+        e.preventDefault()
+        const { classid, freeresponseinput, userid} = this.state;
+        this.setState({
+            studentSurveyText:freeresponseinput
+        })
+        socket.emit('student free response', [freeresponseinput, classid, userid], () => {
+            this.setState({
+                freeresponseinput:''
+            })
+        })
+    }
+    handleInput = (e) =>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+    render(){
+        const { room, userType, thumbsDisable, studentUnderstands, 
+            studentSurveyText, teacherSurveyText, freeresponseinput,
+            studentsurveyinput, teacherThumbText } = this.state;
+        return(
+            <div>
+                <p>Student Lecture View Room {room}</p>
+                {teacherThumbText
+                
+                ?<Form>
+                    <p>{teacherThumbText}</p>
+                    <Button onClick={this.buttonPress} 
+                        disabled={thumbsDisable}>
+                        I get it!
+                    </Button>
+                    <Button onClick={this.buttonPress2}
+                        disabled={thumbsDisable}>
+                        I don't get it.
+                    </Button>
+                </Form>
+                : null
+                }
+                { !studentUnderstands
+                ?<Form>
+                    <p>What don't you understand?</p>
+                    <TextArea onChange={this.handleInput} name='studentsurveyinput' value={studentsurveyinput}/>
+                    <Button onClick={this.sendStudentResponse}>This is my question</Button>
+                    <p>{studentSurveyText}</p>
+                </Form>
+                : null
+                }
+                {
+                    teacherSurveyText
+                    ?<Form>
+                        <p>{teacherSurveyText}</p>
+                        <TextArea onChange={this.handleInput} name='freeresponseinput' value={freeresponseinput}></TextArea>
+                        <Button onClick={this.sendFreeResponse}>Send Response</Button>
+                    </Form>
+                    :null
+                }
+
+
+            </div>
+        )
+    }
+}
+
+export default StudentLecture;
--- src/components/TeacherLecture/TeacherLecture.js
+++ src/components/TeacherLecture/TeacherLecture.js
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 import openSocket from 'socket.io-client';
 import axios from 'axios';
+import { Button, Input, Form, Icon, Label, List, Loader, Segment, Breadcrumb, Grid, Transition, TextArea} from 'semantic-ui-react';
+import './TeacherLecture.css';
 const socket = openSocket(`/`)
-
 class TeacherLecture extends Component {
     constructor(){
         super();
-
         this.state = {
             userType:'Instructor',
-            thumb:'',
             count:0,
             response2:'',
             count2:0,
             classid:1,
             room:'',
-            userid:3, 
-            thumbsDisable:false,
             teachersurveyinput:'',
-            teacherSurveyText:'',
-            studentSurveyText:'',
-            studentResponses:['student responses here'],
-            studentUnderstands:true,
-            studentsurveyinput:''
+            studentFreeResponses:[],
+            teacherthumbinput:'',
+            teacherThumbText:'',
+            studentResponses:[],
         }
         socket.on('thumbcount', thumbquality => {
             if (thumbquality === 'thumbsup')
@@ -37,59 +33,33 @@ class TeacherLecture extends Component {
                 })
             }
         })
-        socket.on('open thumbs', teacherinput => {
-            this.setState({
-                teacherSurveyText:teacherinput,
-                thumbsDisable:false,
-                studentSurveyText:'',
-                studentUnderstands:true
-            })
-        })
         socket.on('get student response', studentinput => {
             let studentArray = [...this.state.studentResponses]
             studentArray.push({question:studentinput[0], studentid:studentinput[2]})
             this.setState({
                 studentResponses:studentArray,
-                
+            })
+        })
+        socket.on('get free response', studentinput => {
+            let freeResponsesArray = [...this.state.studentFreeResponses];
+            freeResponsesArray.push({response:studentinput[0],studentid:studentinput[2]})
+            this.setState({
+                studentFreeResponses:freeResponsesArray
             })
         })
     }
     componentDidMount(){
-        const { socket, classid, userType, count, count2 } = this.state;
-        // socket.on('ping', data => this.setState({response:data}))
-        
-    }
-    componentDidUpdate(prevProps, prevState, snapshot){
-        const { socket, userType } = this.state;
-        
-    }
-
-    buttonPress = () => {
-        const { count, userType, classid, userid} = this.state;
-        socket.emit('students send thumbs', ['thumbsup', classid, userid], () => {
-            this.setState({
-                thumbsDisable:true
-            })
-        })
-    }
-    buttonPress2 = () => {
-        const { count2, classid, userid } = this.state;
-        socket.emit('students send thumbs',['thumbsdown', classid, userid], () => {
-            this.setState({
-                thumbsDisable:true,
-                studentUnderstands:false
-            })
-        })
-    }
-    joinRoom = () => {
         const { userType, classid } = this.state;
         socket.emit('join',`${userType}${classid}`, room => {
             this.setState({
                 room
             })
         })
+        
     }
-   
+    componentDidUpdate(prevProps, prevState, snapshot){
+        const { socket, userType } = this.state;
+    }
     componentWillUnmount(){
         socket.close()
     }
@@ -100,10 +70,10 @@ class TeacherLecture extends Component {
     }
     launchThumbs = (e) =>{
         e.preventDefault()
-        const {classid, teachersurveyinput} = this.state;
-        socket.emit('thumbs launched',[teachersurveyinput, classid], () => {
+        const {classid, teacherthumbinput} = this.state;
+        socket.emit('thumbs launched',[teacherthumbinput, classid], (data) => {
             this.setState({
-                teacherSurveyText:teachersurveyinput,
+                teacherThumbText:teacherthumbinput,
                 count:0,
                 count2:0,
                 teachersurveyinput:'',
@@ -111,97 +81,64 @@ class TeacherLecture extends Component {
             })
         })
     }
-    sendStudentResponse = (e) =>{
+    freeResponse = (e) =>{
         e.preventDefault()
-        const {classid, studentsurveyinput, userid} = this.state;
-        this.setState({
-            studentSurveyText:studentsurveyinput
-        })
-        socket.emit('student response', [studentsurveyinput, classid, userid], () => {
+        const {classid, teachersurveyinput} = this.state;
+        socket.emit('free response', [teachersurveyinput, classid], () => {
             this.setState({
-                studentsurveyinput:''
+                teachersurveyinput:'',
+                studentFreeResponses:[]
             })
         })
+
     }
     
+    
     render(){
-        const { response, count, response2, count2, room, userType, thumbsDisable, studentUnderstands, studentResponses, teacherSurveyText, studentSurveyText} = this.state
+        const {  count, count2, room, studentResponses, 
+            teacherSurveyText, teacherthumbinput, teachersurveyinput, 
+            teacherThumbText, studentFreeResponses} = this.state
 
         return(
             <div>
-                <p>TeacherLecture</p>
-                
-                
+                <p>TeacherLecture Room: {room}</p>
+                <Form>
+                    <p>Thumbs Survey</p>
+                    <TextArea name='teacherthumbinput' value = {teacherthumbinput} onChange={this.handleInput}/>
+                    <Button onClick={ this.launchThumbs}>LAUNCH THE THUMBS</Button>
+                </Form>
                 <div>
-                    <p>{room}</p>
-                    <button onClick={this.joinRoom}>Join Room</button>
+                    <p>thumbsup {count}</p>
+                    <p>thumbsdown {count2}</p>
                 </div>
-                <form>
-                    <input type='radio' value='Student' id='userChoice1' name='userType' onChange={this.handleInput}/>
-                    <label htmlFor='userChoice1'>Student</label>
-                    <input type='radio' value='Instructor' id='userChoice2' name='userType' onChange={this.handleInput}/>
-                    <label htmlFor='userChoice2'>Instructor</label>
-                    <p>userType: {userType}</p>
-                </form>
-                <form>
-                    <input name='userid' value={this.state.userid} id='userid'onChange={this.handleInput} />
-                    <label htmlFor='userid'>Input User ID</label>
-                    <p>User ID: {this.state.userid}</p>
-                </form>
-                { userType === 'Instructor'
-                ?<form>
-                    <input name='teachersurveyinput' value = {this.state.teachersurveyinput} onChange={this.handleInput}/>
-                    <button onClick={ this.launchThumbs}>LAUNCH THE THUMBS</button>
-                </form>
-                : teacherSurveyText
-                
-                    ?<form>
-                        <p>{teacherSurveyText}</p>
-                        
-                    </form>
-                    : null
-                }
+                <Form>
+                    <p>Free Response Question</p>
+                    <TextArea name='teachersurveyinput' value={teachersurveyinput} onChange={this.handleInput}/>
+                    <Button onClick={this.freeResponse}>LAUNCH THE QUESTION</Button>
+                </Form>
                 <div>
-                    { userType === 'Instructor'
-                        ?<p>{response} {count}</p>
-                        : teacherSurveyText
-                            ?<button onClick={this.buttonPress} 
-                            disabled={thumbsDisable}>I get it!</button>
-                            :null
-                    }
-                </div>
-                <div>
-                    {userType === 'Instructor'
-                        ? <p>{response2} {count2}</p>
-                        : teacherSurveyText
-                        ? <button onClick={this.buttonPress2}
-                            disabled={thumbsDisable}
-                            >I don't get it.</button>
+                    <p>{teacherThumbText}</p>
+                    <List>
+                        {studentResponses.length > 0
+                        ?   studentResponses.map( (response, i) => {
+                            return <List.Item key ={`thumbResponse${i}`}>{response.studentid}: {response.question}</List.Item>
+                        })
                         :null
-                    }
+                        }
+                    </List>
                 </div>
-                { !studentUnderstands
-                ?<form>
-                    <p>What don't you understand?</p>
-                    <input onChange={this.handleInput} name='studentsurveyinput' value={this.state.studentsurveyinput}/>
-                    <button onClick={this.sendStudentResponse}>This is my question</button>
-                    <p>{this.state.studentSurveyText}</p>
-                </form>
-                : null}
-                
-                { userType === 'Instructor'
-                ?
                 <div>
+                    <p>Free Responses</p>
                     <p>{teacherSurveyText}</p>
-                    <ul>
-                        {studentResponses.map( (response, i) => {
-                            return <li key ={`response ${i}`}>{response.studentid}: {response.question}</li>
-                        })}
-                    </ul>
+                    <List>
+                        {studentFreeResponses.length > 0
+                         ?   studentFreeResponses.map( (response, i) => {
+                            return <List.Item key={`freeResponse${i}`}>{response.studentid}:{response.response}</List.Item>
+                        })
+                        :null
+                        }
+                    </List>
                 </div>
-                : null
-                }
-                
             </div>
         )
     }
