ce4a6154e1b2cead1a61e5851a8484ca1a5664f6
Tyler <tylerlewisjcg@gmail.com>
2018-05-22 11:50:32 -0600

    grade button conditionally rendered

--- src/components/TeacherAssignmentsView/TeacherAssignmentsView.js
+++ src/components/TeacherAssignmentsView/TeacherAssignmentsView.js
@@ -1,104 +1,149 @@
-import React, { Component } from 'react';
-import { Header, Segment, List, Table, Button, Icon, Modal } from "semantic-ui-react";
+import React, { Component } from "react";
+import {
+  Header,
+  Segment,
+  List,
+  Table,
+  Button,
+  Icon,
+  Modal
+} from "semantic-ui-react";
 import _ from "lodash";
 import { connect } from "react-redux";
 import moment from "moment";
-import axios from 'axios';
+import axios from "axios";
 class TeacherAssignmentsView extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            assignments: [],
-            pointsScored: 0,
-            percentage: 0,
-            letterGrade: '',
-            pointsPossible: 0
-         }
-    }
-    componentDidMount(){
-        axios.get(`/api/courseassignments/${this.props.match.params.assignmentid}`)
-        .then(response =>{
-            this.setState({assignments: response.data})
-        })
-    }
-gradeAssignment(assignmentId, pointsScored, percentage,studentAssignmentId){
-    let letterGrade = this.letterGrade(percentage)
+  constructor(props) {
+    super(props);
+    this.state = {
+      assignments: [],
+      pointsScored: 0,
+      percentage: 0,
+      letterGrade: "",
+      pointsPossible: 0
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(`/api/courseassignments/${this.props.match.params.assignmentid}`)
+      .then(response => {
+        this.setState({ assignments: response.data });
+      });
+  }
+  gradeAssignment(assignmentId, pointsScored, percentage, studentAssignmentId) {
+    let letterGrade = this.letterGrade(percentage);
     let body = {
-        assignmentId,
-        pointsScored: +pointsScored,
-        percentage,
-        letterGrade: letterGrade,
-    
+      assignmentId,
+      pointsScored: +pointsScored,
+      percentage,
+      letterGrade: letterGrade
+    };
+    axios
+      .put(`/api/gradeassignment/${studentAssignmentId}`, body)
+      .then(response => {
+        this.setState({
+          assignments: response.data
+        });
+      });
+  }
+  letterGrade(percentage) {
+    if (percentage >= 90) {
+      return "A";
     }
-axios.put(`/api/gradeassignment/${studentAssignmentId}`, body).then(response =>{
-    this.setState({assignments: response.data
-    })
-})
-}
-letterGrade(percentage){
-    if(percentage >= 90){   
-        return "A"
+    if (percentage >= 80 && percentage < 90) {
+      return "B";
     }
-    if(percentage >= 80 && percentage < 90){      
-        return "B"
+    if (percentage >= 70 && percentage < 80) {
+      return "C";
     }
-    if(percentage >= 70 && percentage < 80){       
-        return "C"
+    if (percentage >= 60 && percentage < 70) {
+      return "D";
     }
-    if(percentage >= 60 && percentage < 70){     
-        return "D"
+    if (percentage < 60) {
+      return "F";
+    } else {
+      return "";
     }
-    if(percentage < 60){     
-        return "F"
-    }
-    else {
-        return ""
-    }
-}
-percentageFinder(pointsScored, pointsPossible){
-    this.setState({percentage: pointsScored * 100 / pointsPossible })
-  
-}
-handlePointsChange(e, pointsPossible){
-    this.percentageFinder(e.target.value, this.state.pointsPossible)
-    let letterGrade = this.letterGrade(this.state.percentage)
-    this.setState({pointsScored: e.target.value,
-        letterGrade: letterGrade,
-        pointsPossible: pointsPossible
-    })
-}
-    render() { 
-      console.log(this.state)
-       let assignments = this.state.assignments.map(assignment =>{
-           
-           return <Table.Row key={assignment.assignment_id + assignment.user_name}>
-           <Table.Cell>{assignment.name}</Table.Cell>
-           <Table.Cell>{assignment.user_name}</Table.Cell>
-           <Table.Cell>{moment(assignment.due_date).format('MM/DD')}</Table.Cell>
-           <Table.Cell>{assignment.percentage? (`${assignment.percentage}%`):  <Modal closeIcon trigger={<Button> Grade</Button>} >
-    <Header content={`${assignment.name} for ${assignment.user_name}`} />
-    <Modal.Content>
-       <p>{`Assignment Description: ${assignment.description}`}</p> 
-    <p>{`Attachment: ${assignment.attachment} `}</p>
-    <p>{`Date Submitted: ${moment(assignment.date_submitted).format('MM/DD')}`}</p>
-    <p>{`Points: `} <input type="number" onChange={(e)=>this.handlePointsChange(e,assignment.points_possible)}/></p>
-    <p>{`Points Possible: ${assignment.points_possible}`}</p>
-    <p>{`Percentage: ${this.state.pointsScored * 100 / assignment.points_possible}%`}</p>
-    <p>{`Letter Grade: ${this.letterGrade(this.state.pointsScored * 100 / assignment.points_possible)}`}</p>
-    </Modal.Content>
-    <Modal.Actions>
-      <Button color='green' onClick={()=>this.gradeAssignment(assignment.assignment_id, this.state.pointsScored, this.state.percentage, assignment.id)} >
-        <Icon name='checkmark' /> Submit Grade
-      </Button>
-    </Modal.Actions>
-  </Modal>}</Table.Cell>
-           
-           
-           </Table.Row>
-       })
-        return ( 
-            <div>
-          <Header as="h2"></Header>
+  }
+  percentageFinder(pointsScored, pointsPossible) {
+    this.setState({
+      percentage: Math.floor(pointsScored * 10000 / pointsPossible) / 100
+    });
+  }
+  handlePointsChange(e, pointsPossible) {
+    this.percentageFinder(e.target.value, this.state.pointsPossible);
+    let letterGrade = this.letterGrade(this.state.percentage);
+    this.setState({
+      pointsScored: e.target.value,
+      letterGrade: letterGrade,
+      pointsPossible: pointsPossible
+    });
+  }
+  render() {
+    console.log(this.state);
+    let assignments = this.state.assignments.map(assignment => {
+      return (
+        <Table.Row key={assignment.assignment_id + assignment.user_name}>
+          <Table.Cell>{assignment.name}</Table.Cell>
+          <Table.Cell>{assignment.user_name}</Table.Cell>
+          <Table.Cell>{moment(assignment.due_date).format("MM/DD")}</Table.Cell>
+          <Table.Cell>
+            {assignment.percentage ? (
+              `Completed Grade: ${assignment.percentage}%`
+            ) : assignment.type === "assignment" ? (
+              <Modal closeIcon trigger={<Button> Grade</Button>}>
+                <Header
+                  content={`${assignment.name} for ${assignment.user_name}`}
+                />
+                <Modal.Content>
+                  <p>{`Assignment Description: ${assignment.description}`}</p>
+                  <p>{`Attachment: ${assignment.attachment} `}</p>
+                  <p>{`Date Submitted: ${moment(
+                    assignment.date_submitted
+                  ).format("MM/DD")}`}</p>
+                  <p>
+                    {`Points: `}{" "}
+                    <input
+                      type="number"
+                      onChange={e =>
+                        this.handlePointsChange(e, assignment.points_possible)
+                      }
+                    />
+                  </p>
+                  <p>{`Points Possible: ${assignment.points_possible}`}</p>
+                  <p>{`Percentage: ${Math.floor(
+                    this.state.pointsScored * 10000 / assignment.points_possible
+                  ) / 100}%`}</p>
+                  <p>{`Letter Grade: ${this.letterGrade(
+                    this.state.pointsScored * 100 / assignment.points_possible
+                  )}`}</p>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button
+                    color="green"
+                    onClick={() =>
+                      this.gradeAssignment(
+                        assignment.assignment_id,
+                        this.state.pointsScored,
+                        this.state.percentage,
+                        assignment.id
+                      )
+                    }
+                  >
+                    <Icon name="checkmark" /> Submit Grade
+                  </Button>
+                </Modal.Actions>
+              </Modal>
+            ) : (
+              "Quiz Not Completed"
+            )}
+          </Table.Cell>
+        </Table.Row>
+      );
+    });
+    return (
+      <div>
+        <Header as="h2" />
         <Table striped>
           <Table.Header>
             <Table.Row>
@@ -108,20 +153,16 @@ handlePointsChange(e, pointsPossible){
               <Table.HeaderCell>Grade</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
-          <Table.Body>
-              {assignments}
-          </Table.Body>
+          <Table.Body>{assignments}</Table.Body>
         </Table>
-       
-            </div>
-         )
-    }
+      </div>
+    );
+  }
 }
 function mapStateToProps(state) {
-    return {
-      courses: state.teachers.courses
-    };
-  }
-  
-  export default connect(mapStateToProps)(TeacherAssignmentsView);
-  
+  return {
+    courses: state.teachers.courses
+  };
+}
+
+export default connect(mapStateToProps)(TeacherAssignmentsView);
--- src/components/TeacherCourseDaySelector/TeacherCourseAssignments.js
+++ src/components/TeacherCourseDaySelector/TeacherCourseAssignments.js
@@ -3,7 +3,7 @@ import { Header, Segment, List, Table } from "semantic-ui-react";
 import _ from "lodash";
 import { connect } from "react-redux";
 import moment from "moment";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 class TeacherCourseAssignments extends Component {
   constructor(props) {
     super(props);
@@ -11,15 +11,13 @@ class TeacherCourseAssignments extends Component {
       course => course.id === this.props.course
     )[0];
     this.state = {
-      currentCourse: filteredCourse,
-
+      currentCourse: filteredCourse
     };
   }
 
   render() {
     const tempAssignments = [];
     const tempQuizzes = [];
- 
 
     if (!this.props.days) {
       return "";
@@ -28,38 +26,44 @@ class TeacherCourseAssignments extends Component {
       let assignment = this.props.days.map(day => {
         return day.assignments;
       });
-      
+
       tempAssignments.push(assignment);
-    
     }
     if (this.props.days) {
       let quiz = this.props.days.map(day => {
         return day.quizzes;
       });
-      
+
       tempQuizzes.push(quiz);
-    
     }
     const assignmentsToMap = _.flattenDeep(tempAssignments);
     const quizzesToMap = _.flattenDeep(tempQuizzes);
-    var uniqueQuizzes =_.uniqBy(quizzesToMap, "description");
+    var uniqueQuizzes = _.uniqBy(quizzesToMap, "description");
     var uniqueAssignments = _.uniqBy(assignmentsToMap, "description");
 
     const assignmentsList = uniqueAssignments.map(assignment => {
       return (
         <Table.Row key={assignment.id + assignment.description}>
-          <Table.Cell> <Link to={`/teacher/courseassignments/${assignment.assignment_id}`}>{assignment.name}</Link> </Table.Cell>
+          <Table.Cell> 
+            <Link to={`/teacher/courseassignments/${assignment.assignment_id}`}>
+              {assignment.name}
+            </Link>
+          </Table.Cell>
           <Table.Cell>{moment(assignment.due_date).format("MM/DD")}</Table.Cell>
         </Table.Row>
       );
-    })
-      const quizzesList = uniqueQuizzes.map(quiz => {
-        return (
-          <Table.Row key={quiz.id + quiz.description}>
-            <Table.Cell> <Link to={`/teacher/courseassignments/${quiz.assignment_id}`}>{quiz.name}</Link> </Table.Cell>
-            <Table.Cell>{moment(quiz.due_date).format("MM/DD")}</Table.Cell>
-          </Table.Row>
-        )
+    });
+    const quizzesList = uniqueQuizzes.map(quiz => {
+      return (
+        <Table.Row key={quiz.id + quiz.description}>
+          <Table.Cell>
+            <Link to={`/teacher/courseassignments/${quiz.assignment_id}`}>
+              {quiz.name}
+            </Link>
+          </Table.Cell>
+          <Table.Cell>{moment(quiz.due_date).format("MM/DD")}</Table.Cell>
+        </Table.Row>
+      );
     });
     return (
       <div>
